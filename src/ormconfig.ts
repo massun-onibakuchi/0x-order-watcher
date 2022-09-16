a89c2d4de3816d0b296d0293bc7ceef6d2c4f8a3
massun-onibakuchi <etherdao0x00@gmail.com>
2022-09-16 11:58:15 +0900

    add validation

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,5 +1,5 @@
 import { ConnectionOptions } from 'typeorm';
-import { POSTGRES_READ_REPLICA_URIS, POSTGRES_URI } from './config';
+import { POSTGRES_URI } from './config';
 
 import {
     KeyValueEntity,
@@ -29,17 +29,6 @@ const config: ConnectionOptions = {
         max: 15,
         statement_timeout: 10000,
     },
-    migrations: ['./lib/migrations/*.js'],
-    ...(POSTGRES_READ_REPLICA_URIS
-        ? {
-              replication: {
-                  master: { url: POSTGRES_URI },
-                  slaves: POSTGRES_READ_REPLICA_URIS.map((r) => ({ url: r })),
-              },
-          }
-        : { url: POSTGRES_URI }),
-    cli: {
-        migrationsDir: 'migrations',
-    },
+    url: POSTGRES_URI,
 };
 module.exports = config;
